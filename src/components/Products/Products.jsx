99698a04f15efd1e540d9f7c4f97402c625134db
p.kamrani <p.kamrani@P-KAMRANI>
2022-05-23 16:45:36 +0430

    Part 133 end

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,27 +1,34 @@
 import ProductForm from "./ProductForm";
-import ProductList from './ProductList'
+import ProductList from "./ProductList";
 import Search from "./Search";
-import { useState } from 'react';
-
+import { useState } from "react";
 
 function Products() {
-const [products,setProducts]=useState([])
+  const [products, setProducts] = useState([]);
 
-
-const addProductList=(item)=>{
-  setProducts((prevState)=>[
-    ...prevState,{
-id:Math.random().toString(),
-...item
-    }
-  ])
-}
+  const addProductList = (item) => {
+    fetch("/product.json", {
+      method: "POst",
+      body: JSON.stringify(item),
+      headers: { "Content-Type": "application/json" },
+    }).then((res) => {
+      res.json().then((resData) => {
+        setProducts((prevState) => [
+          ...prevState,
+          {
+            id: resData.name,
+            ...item,
+          },
+        ]);
+      });
+    });
+  };
   return (
     <div className="App">
       <ProductForm onAddProduct={addProductList} />
       <section>
         <Search />
-        <ProductList onRemoveItem={()=>{}} products={products}/>
+        <ProductList onRemoveItem={() => {}} products={products} />
       </section>
     </div>
   );
